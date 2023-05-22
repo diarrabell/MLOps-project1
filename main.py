eb0525afb0e0c63e21c6579649fb50f89b6f2216
Diarra Bell <dbell@diarras-mbp.lan>
2023-05-21 21:53:14 -0400

    adding readme

---
+++ main.py
@@ -0,0 +1,9 @@
+"""
+This file will contain the main function for the code.
+"""
+
+def main():
+    pass
+
+if __name__ == main:
+    main()
---
+++ readme.md
@@ -0,0 +1,5 @@
+# MLOps project 1
+
+Desccription: Build CLI prediction tool using a pre-trained model
+
+*WIP*
