2769e291401c34a38ced3f4b223485f2fcf40530
Diarra Bell <dbell@diarras-mbp.lan>
2023-06-04 19:49:32 -0400

    testing

--- setup.py
+++ setup.py
@@ -3,3 +3,12 @@ from setuptools import setup, find_packages
 with open("requirements.txt") as f:
     requirements = f.read().splitlines()
 
+setup(
+    name="Emotion Classifier",
+    description="Classify Emotions from Text",
+    packages = find_packages(),
+    install_requires=requirements, 
+    entry_points="""[console_scripts]
+    classifier=main:main""",
+    author="Diarra Bell"
+)
