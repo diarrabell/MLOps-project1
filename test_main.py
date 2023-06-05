7ee347a62887ecd0840b156543fd1678835f2cc0
Diarra Bell <dbell@diarras-mbp.lan>
2023-06-04 23:03:37 -0400

    testing

--- test_main.py
+++ test_main.py
@@ -1,20 +1,28 @@
 import pytest
 from click.testing import CliRunner
-from main import main
+from library.model import classify
 
 # test model to make sure it's working!
 # since this program downloads the model directly from huggingface, the expected results may change if the model is updated.
 def test_classify():
-    runner = CliRunner
-    results = runner.invoke(main, ["--text", "I love this"])
-    assert results.exit_code == 0
-    assert results.output == [[{'label': 'anger', 'score': 0.004419781267642975},
+    results = classify("I love this")
+    assert results == [[{'label': 'anger', 'score': 0.004419781267642975},
   {'label': 'disgust', 'score': 0.0016119900392368436},
   {'label': 'fear', 'score': 0.0004138521908316761},
   {'label': 'joy', 'score': 0.9771687984466553},
   {'label': 'neutral', 'score': 0.005764583125710487},
   {'label': 'sadness', 'score': 0.002092392183840275},
   {'label': 'surprise', 'score': 0.008528688922524452}]]
+#     runner = CliRunner
+#     results = runner.invoke(main, ["--text", "I love this"])
+#     assert results.exit_code == 0
+#     assert results.output == [[{'label': 'anger', 'score': 0.004419781267642975},
+#   {'label': 'disgust', 'score': 0.0016119900392368436},
+#   {'label': 'fear', 'score': 0.0004138521908316761},
+#   {'label': 'joy', 'score': 0.9771687984466553},
+#   {'label': 'neutral', 'score': 0.005764583125710487},
+#   {'label': 'sadness', 'score': 0.002092392183840275},
+#   {'label': 'surprise', 'score': 0.008528688922524452}]]
 
 if __name__ == "__main__":
     test_classify()
